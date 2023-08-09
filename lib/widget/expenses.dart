1ea0b5be1877c925156dc0cdd49e580ebc24ef3b
Rajnish Pandey <piyushwinner1997@gmail.com>
2023-08-09 13:46:34 +0530

    organizes the code and Creates a custom list item with the card & Spacer widget

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
@@ -34,7 +34,9 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('The chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses),),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
