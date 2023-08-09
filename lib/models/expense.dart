5f100a23d67eb59794b2e88f15e62aa8b4221e43
Rajnish Pandey <piyushwinner1997@gmail.com>
2023-08-09 12:56:15 +0530

    using lists inside of lists

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -5,12 +5,12 @@ const uuid = Uuid();
 enum Category { food, travel, leisure, work }
 
 class Expense {
-  Expense(
-      {required this.title,
-      required this.amount,
-      required this.date,
-      required this.category,})
-      : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
