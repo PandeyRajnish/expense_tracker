bb0876433849aff728f3a99d42da3e4976bdcdca
Rajnish Pandey <piyushwinner1997@gmail.com>
2023-09-08 01:21:48 +0530

    Using Icons & Formatting Dates

--- lib/widget/expenses_list/expense_item.dart
+++ lib/widget/expenses_list/expense_item.dart
@@ -14,19 +14,25 @@ class ExpenseItem extends StatelessWidget {
           horizontal: 20,
           vertical: 16,
         ),
-        child: Column(children: [
-          Text(expense.title),
-          const SizedBox(height: 4),
-          Row(children: [
-            Text('\$${expense.amount.toStringAsFixed(2)}'),
-            const Spacer(),
-            Row(children: [
-              const Icon(Icons.alarm),
-              const SizedBox(height: 8),
-              Text(expense.date.toString()),
-            ],),
-          ],),
-        ],),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(height: 8),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
