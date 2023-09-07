ac9cb98a54fcf3c309457fd99bc1bbb8dac30ef0
Rajnish Pandey <piyushwinner1997@gmail.com>
2023-09-08 03:17:54 +0530

    Handle User(Text) Input with the TextField Widget

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widget/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
@@ -31,7 +32,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const Text('Modal bottom sheet'),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
